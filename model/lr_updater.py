ee9e8b320bb9262a1572b704b43c73c2e9f2c1f5
Wei Ye <weiye@fb.com>
2023-02-01 14:46:13 -0800

    [fix] add MultiGammaLrUpdaterHook back

---
+++ model/lr_updater.py
@@ -0,0 +1,34 @@
+from mmcv.runner import HOOKS, LrUpdaterHook
+
+
+@HOOKS.register_module()
+class MultiGammaLrUpdaterHook(LrUpdaterHook):
+    """Step LR scheduler.
+
+    Args:
+        step (list[int]): Step to decay the LR. If an int value is given,
+            regard it as the decay interval. If a list is given, decay LR at
+            these steps.
+        gamma (list[float]): LR change ratios at certain steps.
+    """
+
+    def __init__(self, step, gamma, **kwargs):
+        assert mmcv.is_list_of(step, int)
+        assert mmcv.is_list_of(gamma, float)
+        assert len(gamma) == len(step)
+        assert all([s > 0 for s in step])
+        self.step = step
+        self.gamma = gamma
+        super(MultiGammaLrUpdaterHook, self).__init__(**kwargs)
+
+    def get_lr(self, runner, base_lr):
+        progress = runner.epoch if self.by_epoch else runner.iter
+
+        # calculate exponential term
+        gamma = 1
+        for i, s in enumerate(self.step):
+            if progress < s:
+                break
+            gamma *= self.gamma[i]
+
+        return base_lr * gamma
